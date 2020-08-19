75eba036050df98bb4567e2847245c240558040c
Parimaladevi <Parimaladevi@MININT-QOPL6NB.us.corp.planetart.com>
2020-08-18 19:43:17 -0700

    2nd

--- src/jusJava/myCode1.java
+++ src/jusJava/myCode1.java
@@ -5,7 +5,7 @@ public class myCode1 {
 	public static void main(String[] args) {
 
 		System.out.println("Just Java");
-		System.out.println("Added by Pari");
+		System.out.println("Added by Pari...");
 	}
 
 }
