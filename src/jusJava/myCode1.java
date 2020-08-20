23cd6b247088f6c76c2920b434b9d31bf1503979
karth <kk@varkasa.com>
2020-08-19 18:57:22 -0700

    KK accepted ... print msg

--- src/jusJava/myCode1.java
+++ src/jusJava/myCode1.java
@@ -7,6 +7,7 @@ public class myCode1 {
 		System.out.println("Just Java");
 		System.out.println("Added by Pari...");
 		System.out.println("Updated the 3rd print line in order to test commit and push");
+		System.out.println("Thank you team.");
 	}
 
 }
