078954fc30bce4fb9445ec2c4ba0c84a98890120
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-20 15:00:06 -0700

    Made change to my name

--- src/jusJava/myCode1.java
+++ src/jusJava/myCode1.java
@@ -8,6 +8,7 @@ public class myCode1 {
 		System.out.println("Added by Pari...");
 		System.out.println("Updated the 3rd print line in order to test commit and push");
 		System.out.println("Thank you team.");
+		System.out.println("Added by Arthy S Ramalingam.....");
 	}
 
 }
