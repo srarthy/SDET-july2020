07133701afac905fd6d743d94120d98ce1d29d75
srarthy <64612018+srarthy@users.noreply.github.com>
2020-08-25 18:50:24 -0700

    Create myCode2.java

---
+++ myCode2.java
@@ -0,0 +1,15 @@
+public class myCode2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Just Java");
+		System.out.println("Added by Pari...");
+		System.out.println("Updated the 3rd print line in order to test commit and push");
+		System.out.println("Thank you team.");
+		System.out.println("Added by Arthy S Ramalingam.....");
+		System.out.println("Another addition by Arthy.....");
+		System.out.println("Last line added");
+		System.out.println("New Last line added now.See you soon.Love you............");
+	}
+
+}
